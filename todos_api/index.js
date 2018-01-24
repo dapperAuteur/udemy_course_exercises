79ee382b0ce96461b147bd7897ae159cdb597e34
Anthony McDonald <a@awews.com>
2018-01-24 15:47:15 -0500

    full CRUD, getting cors error for put and delete

--- todos_api/index.js
+++ todos_api/index.js
@@ -1,14 +1,21 @@
 var express = require('express'),
     app = express(),
-    port = process.env.PORT || 3000,
+    port = process.env.PORT || 3030,
     bodyParser = require('body-parser');
-    
+
 var todoRoutes = require("./routes/todos");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname +'/public'));
 app.use(express.static(__dirname + '/views'));
+// added to resolve CORS issue
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+  // headers.add("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+});
 
 app.get('/', function(req, res){
     res.sendFile("index.html");
@@ -17,7 +24,5 @@ app.get('/', function(req, res){
 app.use('/api/todos', todoRoutes);
 
 app.listen(port, function(){
-    console.log("APP IS RUNNING ON PORT " + process.env.PORT);
+    console.log("APP IS RUNNING ON PORT " + port);
 })
-    
-    
